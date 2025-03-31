636d45a9cd697e2aa65df74093756295ad7123e3
AukseMaz <aukse.mazeikiene@gmail.com>
2025-03-31 09:23:51 +0300

    Modify Git Actions

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,9 +1,35 @@
 /// <reference types="cypress" />
 
-describe('Sweet Shop spec', () => {
-    const baseUrl = 'https://sweetshop.netlify.app/';
+describe('TS_7 Checkout Process', () => {
+    context('TC_7.1 Checkout Page Load', () => {
+      beforeEach(() => {
+        cy.visitSweetsPage();
+      });
   
-    beforeEach(() => {
-        cy.visit(baseUrl); // Visit the main page before each test
+      it('TC_7.1.1 Verify the checkout page is accessible', () => {
+        cy.get(".col-lg-3").each(($product) => {
+            cy.verifyProductDetails($product); // Check product details
+            cy.addProductToBasket($product); // Click 'Add to Basket'
+        });
+  
+        // Navigate to the Basket page
+        cy.navigateToPage('Basket', '/basket', 'Your Basket');
+  
+        // Click on the "Continue to checkout" button
+        cy.contains('Continue to checkout').click();
+  
+        // Verify that the user is redirected to the Checkout page
+        cy.url().should('include', '/checkout');
+  
+        // Verify the presence of the billing address form
+        cy.get('form#billing-address').should('be.visible');
+  
+        // Verify the presence of the payment section
+        cy.get('section#payment').should('be.visible');
+  
+        // Verify the presence of the order summary
+        cy.get('section#order-summary').should('be.visible');
+      });
     });
   });
+  

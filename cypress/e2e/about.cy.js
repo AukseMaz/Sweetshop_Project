e9c0c43760699899ed19b97df9035cdeff65597c
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-02 09:52:10 +0300

    Add variables to about.cy.js file

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
 describe("SweetShop - About Page", () => {
+    const paragraphsDescription = ".lead";
+    const paragraphFooterText = ".m-0.text-center";
+
     beforeEach(() => {
         cy.visitAboutPage();
     });
@@ -10,12 +13,12 @@ describe("SweetShop - About Page", () => {
     });
   
     it("TC_1.2 Verify the page has a description", () => {
-        cy.get(".lead").should("exist").and("be.visible").and("not.be.empty");
+        cy.get(paragraphsDescription).should("exist").and("be.visible").and("not.be.empty");
     });
   
     it("TC_1.3 Verify the page has a footer and it matches the year 2018", () => {
-        cy.get(".m-0.text-center").should("be.visible");
-        cy.contains(".m-0.text-center", "2018").should("be.visible");
+        cy.get(paragraphFooterText).should("be.visible");
+        cy.contains(paragraphFooterText, "2018").should("be.visible");
     });
 });
 

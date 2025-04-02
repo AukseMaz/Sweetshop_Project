2ddc4d698059995db48f7c9e1627f808698d973b
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-02 09:58:31 +0300

    Add variable to account.cy.js file

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Sweet Shop - Your Account', () => {
-    const url = 'https://sweetshop.netlify.app/00efc23d-b605-4f31-b97b-6bb276de447e.html';
+  const positiveLogin = "p.lead";
+    const url = "https://sweetshop.netlify.app/00efc23d-b605-4f31-b97b-6bb276de447e.html";
     beforeEach(() => {
         cy.visit(url); 
     });
 
     it('TC_5.1: Positive login, verify user info is displayed.', () => {
-        // Step 1: Verify the user’s name displayed
-        cy.get('p.lead') // Select the paragraph element containing the welcome message
-          .should('be.visible') 
-          .and('contain.text', 'Welcome back test@user.com'); 
+     
+        cy.get(positiveLogin) 
+          .should("be.visible") 
+          .and("contain.text", "Welcome back test@user.com"); 
     });
   });

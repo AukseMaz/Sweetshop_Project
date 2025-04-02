a518d771c4bbb7d9da664f3484b75440d40caf45
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-02 11:05:01 +0300

    Add variables to navigationBar.cy.js file

--- cypress/e2e/navigationBar.cy.js
+++ cypress/e2e/navigationBar.cy.js
@@ -1,39 +1,43 @@
 /// <reference types="cypress" />
 
 describe('Pages load from main page correctly.', () => {
+    const navigationBar = ".collapse.navbar-collapse";
+
     beforeEach(() => {
         cy.visitMainPage();
     });
 
     it('TC_1.1 Should navigate to the "Sweets" page correctly', () => {
-            // Click on the "Sweets" navigation link
-            cy.get('.collapse.navbar-collapse').contains('Sweets').click();
-            // Verify the new URL contains "sweets"
-            cy.url().should('include', '/sweets');
-            // Verify the Sweets Page heading or content is available
-            cy.get('h1.display-3').should('contain.text', 'Browse sweets');
-        });
+        // Click on the "Sweets" navigation link
+        cy.get(navigationBar).contains('Sweets').click();
+        // Verify the new URL contains "sweets"
+        cy.url().should('include', '/sweets');
+        // Verify the Sweets Page heading or content is available
+        cy.get('h1.display-3').should('contain.text', 'Browse sweets');
+    });
 
     it('TC_1.2 Should navigate to the "About" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('About').click();
+        cy.get(navigationBar).contains('About').click();
         cy.url().should('include', '/about');
         cy.get('h1.display-3').should('contain.text', 'Sweet Shop Project');
     });
 
     it('TC_1.3 Should navigate to the "Login" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Login').click();
+        cy.get(navigationBar).contains('Login').click();
         cy.url().should('include', '/login');
         cy.get('h1.display-3').should('contain.text', 'Login');
     });
 
     it('TC_1.4 Should navigate to the "Basket" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Basket').click();
+        cy.get(navigationBar).contains('Basket').click();
         cy.url().should('include', '/basket');
         cy.get('h1.display-3').should('contain.text', 'Your Basket');
     });
 });
 
 describe('Pages load from "Sweets" page correctly.', () => {
+    const navigationBar = ".collapse.navbar-collapse";
+
     beforeEach(() => {
         cy.visitSweetsPage();
     });
@@ -45,25 +49,27 @@ describe('Pages load from "Sweets" page correctly.', () => {
     });
 
     it('TC_1.6 Should navigate to the "About" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('About').click();
+        cy.get(navigationBar).contains('About').click();
         cy.url().should('include', '/about');
         cy.get('h1.display-3').should('contain.text', 'Sweet Shop Project');
     });
 
     it('TC_1.7 Should navigate to the "Login" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Login').click();
+        cy.get(navigationBar).contains('Login').click();
         cy.url().should('include', '/login');
         cy.get('h1.display-3').should('contain.text', 'Login');
     });
 
     it('TC_1.8 Should navigate to the "Basket" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Basket').click();
+        cy.get(navigationBar).contains('Basket').click();
         cy.url().should('include', '/basket');
         cy.get('h1.display-3').should('contain.text', 'Your Basket');
     });
 });
 
 describe('Pages load from "About" page correctly.', () => {
+    const navigationBar = ".collapse.navbar-collapse";
+
     beforeEach(() => {
         cy.visitAboutPage();
     });
@@ -75,25 +81,27 @@ describe('Pages load from "About" page correctly.', () => {
     });
 
     it('TC_1.10 Should navigate to the "Sweets" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Sweets').click();
+        cy.get(navigationBar).contains('Sweets').click();
         cy.url().should('include', '/sweets');
         cy.get('h1.display-3').should('contain.text', 'Browse sweets');
     });
 
     it('TC_1.11 Should navigate to the "Login" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Login').click();
+        cy.get(navigationBar).contains('Login').click();
         cy.url().should('include', '/login');
         cy.get('h1.display-3').should('contain.text', 'Login');
     });
 
     it('TC_1.12 Should navigate to the "Basket" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Basket').click();
+        cy.get(navigationBar).contains('Basket').click();
         cy.url().should('include', '/basket');
         cy.get('h1.display-3').should('contain.text', 'Your Basket');
     });
 });
 
 describe('Pages load from "Login" page correctly.', () => {
+    const navigationBar = ".collapse.navbar-collapse";
+
     beforeEach(() => {
         cy.visitLoginPage();
     });
@@ -105,25 +113,27 @@ describe('Pages load from "Login" page correctly.', () => {
     });
 
     it('TC_1.14 Should navigate to the "Sweets" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Sweets').click();
+        cy.get(navigationBar).contains('Sweets').click();
         cy.url().should('include', '/sweets');
         cy.get('h1.display-3').should('contain.text', 'Browse sweets');
     });
 
     it('TC_1.15 Should navigate to the "About" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('About').click();
+        cy.get(navigationBar).contains('About').click();
         cy.url().should('include', '/about');
         cy.get('h1.display-3').should('contain.text', 'Sweet Shop Project');
     });
 
     it('TC_1.16 Should navigate to the "Basket" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Basket').click();
+        cy.get(navigationBar).contains('Basket').click();
         cy.url().should('include', '/basket');
         cy.get('h1.display-3').should('contain.text', 'Your Basket');
     });
 });
 
 describe('Pages load from "Basket" page correctly.', () => {
+    const navigationBar = ".collapse.navbar-collapse";
+
     beforeEach(() => {
         cy.visitBasketPage();
     });
@@ -135,27 +145,34 @@ describe('Pages load from "Basket" page correctly.', () => {
     });
 
     it('TC_1.18 Should navigate to the "Sweets" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Sweets').click();
+        cy.get(navigationBar).contains('Sweets').click();
         cy.url().should('include', '/sweets');
         cy.get('h1.display-3').should('contain.text', 'Browse sweets');
     });
 
     it('TC_1.19 Should navigate to the "About" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('About').click();
+        cy.get(navigationBar).contains('About').click();
         cy.url().should('include', '/about');
         cy.get('h1.display-3').should('contain.text', 'Sweet Shop Project');
     });
 
     it('TC_1.20 Should navigate to the "Login" page correctly', () => {
-        cy.get('.collapse.navbar-collapse').contains('Login').click();
+        cy.get(navigationBar).contains('Login').click();
         cy.url().should('include', '/login');
         cy.get('h1.display-3').should('contain.text', 'Login');
     });
-    
+});
+
+describe('Sweet Shop Logo', () => {
+    const logoImage = 'img[src="favicon.png"]';
+
+    before(() => {
+        cy.visitMainPage();
+    });
     it('TC_1.21 Verify that the logo icon is visible', () => {
-        // Step 1: Check if the logo image exists
-        cy.get('img[src="favicon.png"]') // Select the image based on its src attribute
-          .should('exist') // Assert that the image exists in the DOM
-          .and('be.visible');
+        // Check if the logo image exists
+        cy.get(logoImage)
+            .should('exist')
+            .and('be.visible');
     });
 });

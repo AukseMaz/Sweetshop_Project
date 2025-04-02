6632ae16ca52727c4e374c58899daa24e78606fa
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-02 17:40:58 +0300

    Modify commands.js and checkout.js files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,19 +20,35 @@ Cypress.Commands.add("visitBasketPage", () => {
 
 Cypress.Commands.add('addItemToBasket', (itemIndex = 0) => {
     cy.get('.col-lg-3').eq(itemIndex).find('a.addItem').click();
-  });
+});
 
-  Cypress.Commands.add("addRandomItemsToBasket", (minItems, maxItems) => {
+Cypress.Commands.add("addRandomItemsToBasket", (minItems, maxItems) => {
     cy.get(".btn").then(($buttons) => {
         const totalItems = $buttons.length;
-        const numberOfItems = Cypress._.random(minItems, maxItems); 
-        const randomIndexes = Cypress._.shuffle([...Array(totalItems).keys()]).slice(0, numberOfItems); 
+        const numberOfItems = Cypress._.random(minItems, maxItems);
+        const randomIndexes = Cypress._.shuffle([...Array(totalItems).keys()]).slice(0, numberOfItems);
 
         randomIndexes.forEach((index) => {
             cy.wrap($buttons.eq(index)).click();
         });
         // Store count for later assertions
-        cy.wrap(numberOfItems).as("selectedItemCount"); 
+        cy.wrap(numberOfItems).as("selectedItemCount");
     });
 });
+
+Cypress.Commands.add('fillCheckoutForm', (email, firstName, lastName, address, address2, country, city, zip, cardName, cardNumber, expiration, cvv) => {
+    cy.get("input#name").first().clear().type(firstName);
+    cy.get("input#name").last().clear().type(lastName);
+    cy.get("input#email").clear().type(email);
+    cy.get("input#address").clear().type(address);
+    cy.get("input#address2").clear().type(address2);
+    cy.get("select#country").select(country);
+    cy.get("select#city").select(city);
+    cy.get("input#zip").clear().type(zip);
+    cy.get("input#cc-name").clear().type(cardName);
+    cy.get("input#cc-number").clear().type(cardNumber);
+    cy.get("input#cc-expiration").clear().type(expiration);
+    cy.get("input#cc-cvv").clear().type(cvv);
+});
+
 

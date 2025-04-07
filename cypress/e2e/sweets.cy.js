b72197e5e0816658f8a294d978df9b3d29d7dd82
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-07 14:01:24 +0300

    Modify sweets.cy.js file

--- cypress/e2e/sweets.cy.js
+++ cypress/e2e/sweets.cy.js
@@ -16,13 +16,10 @@ describe("Sweet Shop - Sweets Page", () => {
     });
 
     it("TC_2.2 Verify the list of available products is displayed.", () => {
-        // Verify that a list of products is displayed on the page
         cy.get(productList).should("exist");
 
-        // Check that multiple products are listed (not an empty page)
         cy.get(availableProductList).should("have.length.greaterThan", 0);
 
-        // Verify specific product details (like names or images)
         cy.get(availableProductList).each(($el) => {
             cy.wrap($el).find(".card-title").should("exist"); 
             cy.wrap($el).find(".card-img-top").should("have.attr", "src").and("not.be.empty"); 
@@ -30,33 +27,25 @@ describe("Sweet Shop - Sweets Page", () => {
     });
 
     it('TC_2.3 Verify each product has a name, price, image and "Add to Basket" button.', () => {
-        // Verify that a list of products is displayed on the page
         cy.get(productList).should("exist");
 
-        // For each displayed product, verify that it contains:
         cy.get(availableProductList).each(($product) => {
-            // Check for product name
             cy.wrap($product).find(".card-title").should("exist");
 
-            // Check for product price
             cy.wrap($product).find("p").should("exist");
             cy.wrap($product).find(productPrice)
                 .should("exist")
                 .and("contain.text", "£");
 
-            // Iterate over each product
             cy.get(availableProductList).each(($product) => {
-                // Check for product image
                 cy.wrap($product).find(".card-img-top")
                     .should("have.attr", "src")
                     .and("not.be.empty")
                     .then(($img) => {
-                        // Ensure the image has loaded successfully
                         expect($img[0].naturalWidth).to.be.greaterThan(0);
                     });
             });
 
-            // Check for "Add to Basket" button
             cy.wrap($product).find("a.addItem").should("be.visible").and("contain.text", "Add to Basket");
             cy.wrap($product).find("a.addItem").click();
         });

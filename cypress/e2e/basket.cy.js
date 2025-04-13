c010f78271a65d55231615346f05c9f31ed65a24
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-13 19:14:39 +0300

    Modify basket.cy.js file

--- cypress/e2e/basket.cy.js
+++ cypress/e2e/basket.cy.js
@@ -7,6 +7,7 @@ describe("Basket Functionality - Adding Products", () => {
     const itemDeleteLink = "a.small";
     const emptyTotal = "li.list-group-item.d-flex.justify-content-between";
     const basketCountBadge = "span.badge, span.badge-pill";
+    const completeBasket = 'ul[id="basketItems"]';
 
     beforeEach(() => {
         cy.visitSweetsPage();
@@ -25,7 +26,7 @@ describe("Basket Functionality - Adding Products", () => {
 
         cy.visitBasketPage();
         cy.get(deliveryCollectFree).click();
-        cy.get(".list-group").should("be.visible");
+        cy.get(completeBasket).should("be.visible");
     });
 
     it('TC_6.3 Add random products and select "Standard shipping (£1.99)".', () => {
@@ -50,7 +51,7 @@ describe("Basket Functionality - Adding Products", () => {
     });
 
     it("TC_6.4 Remove an item from the basket and verify count is updated.", () => {
-        cy.addRandomItemsToBasket(3, 5); 
+        cy.addRandomItemsToBasket(3, 5);
         cy.visitBasketPage();
 
         cy.get("@selectedItemCount").then((initialCount) => {
@@ -59,39 +60,33 @@ describe("Basket Functionality - Adding Products", () => {
             cy.get(itemDeleteLink).first().click();
             cy.on("window:confirm", () => true);
 
-            cy.get(basketCountBadge).invoke("text").then((text) => {
-                const updatedCount = parseInt(text.trim());
-                expect(updatedCount).to.equal(initialCount - 1);
-            });
+            cy.get(basketCountBadge)
+                .invoke("text")
+                .then((countText) => {
+                    const count = parseInt(countText.trim());
+                    cy.wrap(count).as("selectedItemCount");
+                });
         });
     });
 
     it("TC_6.5 Empty basket and verify it is empty.", () => {
-        cy.addRandomItemsToBasket(3, 5); 
+        cy.addRandomItemsToBasket(3, 5);
         cy.visitBasketPage();
 
-        cy.get("body").then(($body) => {
-            if ($body.find(itemDeleteLink).length > 0) {
-                const deleteItem = () => {
-                    cy.get("body").then(($body) => {
-                        if ($body.find(itemDeleteLink).length > 0) {
-                            cy.get(itemDeleteLink).first().click();
-                            cy.on("window:confirm", () => true);
-                            cy.wait(500);
-                            cy.reload();
-                            cy.wait(1000);
-                            deleteItem();
-                        } else {
-                            cy.log("Basket is empty");
-                        }
-                    });
-                };
-                deleteItem();
-            } else {
-                cy.log("Basket was already empty");
-            }
-        });
+        function deleteAllItems() {
+            cy.get("body").then(($body) => {
+                if ($body.find(itemDeleteLink).length > 0) {
+                    cy.get(itemDeleteLink).first().click();
+                    cy.on("window:confirm", () => true);
+                    cy.wait(500);
+                    cy.reload();
+                    cy.wait(500);
+                    deleteAllItems();
+                }
+            });
+        }
 
+        deleteAllItems();
         cy.get(".basketCount", { timeout: 10000 }).should("not.exist");
     });
 
@@ -99,28 +94,17 @@ describe("Basket Functionality - Adding Products", () => {
         cy.addRandomItemsToBasket(3, 5);
         cy.visitBasketPage();
 
-        cy.get("body").then(($body) => {
-            const emptyBasketLink = $body.find('a[href="#"]').filter(function () {
-                return Cypress.$(this).text().includes("Empty Basket");
+        cy.contains('a', 'Empty Basket').click();
+        cy.on("window:confirm", () => true);
+
+        cy.get(basketCountBadge).should("contain.text", "0");
+
+        cy.get(emptyTotal).within(() => {
+            cy.get("span").should("contain.text", "Total (GBP)");
+            cy.get("strong").invoke("text").then((totalText) => {
+                const total = totalText.replace("£", "").trim();
+                expect(Number(total)).to.equal(0.00);
             });
-
-            if (emptyBasketLink.length > 0) {
-                cy.wrap(emptyBasketLink).click();
-                cy.on("window:confirm", () => true);
-                cy.wait(500);
-
-                cy.get(basketCountBadge).should("contain.text", "0");
-
-                cy.get(emptyTotal).within(() => {
-                    cy.get("span").should("contain.text", "Total (GBP)");
-                    cy.get("strong").invoke("text").then((totalText) => {
-                        const total = totalText.replace("£", "").trim();
-                        expect(Number(total)).to.equal(0.00);
-                    });
-                });
-            } else {
-                cy.log("Basket is already empty");
-            }
         });
     });
 });

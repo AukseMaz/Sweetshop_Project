0e9725e4bfa53e03ddedb44fddd88c8ddc6ba21a
AukseMaz <aukse.mazeikiene@gmail.com>
2025-04-14 13:25:59 +0300

    Modify about.cy.js file

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -12,12 +12,20 @@ describe("SweetShop - About Page", () => {
         cy.contains("h1", "Sweet Shop Project").should("be.visible");
     });
   
-    it("TC_1.2 Verify the page has a description", () => {
-        cy.get(paragraphsDescription).should("exist").and("be.visible").and("not.be.empty");
-    });
+    it("TC_1.2 Verify the page has the correct description", () => {
+        cy.get(paragraphsDescription)
+            .should("exist")
+            .and("be.visible")
+            .invoke('text')
+            .then((text) => {
+                const normalizedText = text.replace(/\s+/g, ' ').trim();
+                expect(normalizedText).to.equal(
+                    "An intentionally broken web application to help demonstrate Chrome DevTools. Sweet Shop is a project created to help demonstrate some of the great features of the Chrome DevTools which may be of use to people who help test web applications. Sweet Shop encompasses common issues found in real-world web applications!"
+                );
+            });
+    });      
   
     it("TC_1.3 Verify the page has a footer and it matches the year 2018", () => {
-        cy.get(paragraphFooterText).should("be.visible");
         cy.contains(paragraphFooterText, "2018").should("be.visible");
     });
 });
